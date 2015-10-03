f17155d31684252fe91e3f35bd89df421bca5049
Mathew Teoh <mteoh@live.ca>
2015-10-03 11:27:28 -0700

    another sad attempt dont worry nothing here

--- compareTest.py
+++ compareTest.py
@@ -12,17 +12,28 @@ def parseTestFile():
 			# print(row[0])
 			testpeptide = remove_dat_hoohah(row[0])
 			scannum = row[6]
-			print(testpeptide)
-			print(scannum)
+			# print(testpeptide)
+			# print(scannum)
 			testscanpeps.append([scannum,testpeptide])
 	return testscanpeps
 
 def remove_dat_hoohah(testpeptide):
 	return testpeptide.replace("(+57.02)","")
 
+def parseResultsFile():
+	resultpeps = []
+	with open('results.csv','rt') as csvfile:
+		resread = csv.reader(csvfile,delimiter='\t')
+		for row in resread:
+			if(row[0] != 'None'):
+				chosenpep = row[0]
+				candidatepep = row[2]
+				print(candidatepep)
+
 def main():
 	test_scanpeps = parseTestFile()
-	
+	parseResultsFile()
+
 
 if __name__ == "__main__":
 	main()
